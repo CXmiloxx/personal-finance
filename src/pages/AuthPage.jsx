1f308ce3af2410da12470f4fea4a4425ed1ee744
Camilo <juancamilog9911@gmail.com>
2025-04-09 22:34:31 -0500

    ✨ feat: agrega página de autenticación con componentes de registro y login

---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,12 @@
+import Login from '@/user-auth/Login';
+import SignUp from '@/user-auth/SignUp';
+
+export default function AuthPage() {
+  return (
+    
+    <>
+      <SignUp />
+      <Login />
+    </>
+  );
+}
