39382f6265161bb25863ad392c3f8f2689c29f93
Camilo <juancamilog9911@gmail.com>
2025-04-12 13:18:02 -0500

    💄 style: ajusta las animaciones del botón de Logout para mejorar la experiencia del usuario

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -5,10 +5,13 @@ import { FaSignOutAlt } from 'react-icons/fa';
 export default function Logout({ click }) {
   return (
     <motion.button
-      whileTap={{ scale: 0.85 }}
-      whileHover={{ scale: 1.1, rotate: 10 }}
       onClick={click}
-      className="p-2 rounded-full shadow-lg bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white transition-all duration-300"
+      className="p-2 rounded-full shadow-lg bg-gray-200 dark:bg-gray-800 text-gray-900 
+      dark:text-white transition-all duration-300
+      hover:bg-gray-300 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500
+      "
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.98 }}
     >
       <FaSignOutAlt size={24} className="text-blue-500" />
     </motion.button>
