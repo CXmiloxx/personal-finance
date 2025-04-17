b11720658fd0658d1fd6dd5b8caa07d6e9f0a463
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-04-17 15:39:59 -0500

    ✨ feat: añade rutas para restablecer y crear nueva contraseña

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,8 @@ import Option1Page from '$/Option1Page';
 import CategorysPage from '$/CategoriesPage/CategorysPage';
 import SignUp from '$/AuthPage/components/user-auth/SignUp';
 import Login from '$/AuthPage/components/user-auth/Login';
+import ResetPassword from '$/AuthPage/components/user-auth/ResetPassword';
+import NewPassword from '$/AuthPage/components/user-auth/NewPassword';
 
 export const publicRoutes = [
   {
@@ -17,6 +19,16 @@ export const publicRoutes = [
     path: '/login',
     component: Login,
   },
+  {
+    name: 'Reset Password',
+    path: '/reset-password',
+    component: ResetPassword ,
+  },
+  {
+    name: 'Nueva Contraseña',
+    path: '/new-password/:idUser',
+    component: NewPassword,
+  }
 ];
 
 export const protectedRoutes = [
