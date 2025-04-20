ceca271d4bc94b46e626e66dae2572ec31f6d97e
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-04-20 16:34:36 -0500

    💄 Style:  mejora animaciones y estilo visual del Sidebar
    se cambia el diseño del sidebar cambiando el diseño del borde para que sea como linea descontinua, se usa clsx para las condiciones en las clases para tener una mejor estructura

--- src/components/sideBar/Sidebar.jsx
+++ src/components/sideBar/Sidebar.jsx
@@ -1,26 +1,41 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react';
 import { motion } from 'framer-motion';
+import clsx from 'clsx';
 import SidebarHeader from './SidebarHeader';
 import SidebarNavigation from './SidebarNavigation';
 import { useSidebarState } from '!/useSidebar';
 
 export default function Sidebar() {
-  const {isOpen} = useSidebarState();
-
+  const { isOpen } = useSidebarState();
 
   return (
     <motion.div
       initial={{ width: 60 }}
-      animate={{ width: isOpen ? 240 : 60 }}
-      transition={{ duration: 0.4, ease: 'easeInOut' }}
-      className="h-screen bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text fixed left-0 top-0 shadow-2xl flex flex-col"
+      animate={{ width: isOpen ? 260 : 60 }}
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+      className={clsx(
+        'h-screen fixed top-0 left-0 z-50 flex flex-col justify-between',
+        'shadow-xl border-r-2 border-dashed backdrop-blur-lg',
+        'bg-white/30 text-light-text border-black/70',
+        'dark:bg-dark-bg/60 dark:text-dark-text dark:border-white/80',
+      )}
     >
-      <SidebarHeader />
+      <motion.div
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2, duration: 0.5 }}
+      >
+        <SidebarHeader />
+      </motion.div>
 
-      <SidebarNavigation
-      />
-
+      <motion.div
+        initial={{ opacity: 0, x: -10 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.3, duration: 0.4 }}
+        className={clsx('flex-1 overflow-y-auto px-0 py-4')}
+      >
+        <SidebarNavigation />
+      </motion.div>
     </motion.div>
   );
 }
