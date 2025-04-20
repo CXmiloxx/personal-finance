d7fa6273042b8f9b3685ef92e5ba9bc0bccc48e7
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-04-20 18:00:04 -0500

    💄 Style: reemplaza Option1Page con HomePage en las rutas protegidas y actualiza el nombre de la ruta a "DashBoard"

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,12 @@
 import { FaHome, FaInfoCircle, FaConciergeBell, FaCog } from 'react-icons/fa';
 import AboutPage from '$/AboutPage';
 import ServicesPage from '$/ServicesPage';
-import Option1Page from '$/Option1Page';
 import CategorysPage from '$/CategoriesPage/CategorysPage';
 import SignUp from '$/AuthPage/components/user-auth/SignUp';
 import Login from '$/AuthPage/components/user-auth/Login';
 import ResetPassword from '$/AuthPage/components/user-auth/ResetPassword';
 import NewPassword from '$/AuthPage/components/user-auth/NewPassword';
+import HomePage from '$/HomePage/HomePage';
 
 export const publicRoutes = [
   {
@@ -51,9 +51,9 @@ export const protectedRoutes = [
     component: ServicesPage,
   },
   {
-    name: 'Halloween 2022',
-    path: '/halloween2022',
+    name: 'DashBoard',
+    path: '/dashboard',
     icon: FaCog,
-    component: Option1Page,
+    component: HomePage,
   },
 ];
