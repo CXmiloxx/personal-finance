be24bf812d235631572fff1cdf3f6f68beb9e735
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-10 17:55:47 -0500

    ♻️ refactor(Login): actualiza la redirección tras el inicio de sesión a '/dashboard'

--- src/pages/AuthPage/components/user-auth/Login.jsx
+++ src/pages/AuthPage/components/user-auth/Login.jsx
@@ -22,7 +22,7 @@ export default function Login() {
     e.preventDefault();
     try {
       await login(userData);
-      navigate('/categorias');
+      navigate('/dashboard');
     } catch (err) {
       console.error('Error al iniciar sesión:', err);
       setUserData({ email: '', password: '' });
