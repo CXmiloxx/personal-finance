56cabe0a24164484d0441262589b497630b37c9a
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-06-08 11:08:48 -0500

    ♻️ refactor(routes): elimina importación de FaDollarSign y expensesIncomePage
    - Se eliminan importaciones no utilizadas en el archivo de rutas para limpiar el código y mejorar la legibilidad.

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,4 @@
 import {
-  FaDollarSign,
   FaList,
   FaPlusCircle,
   FaTachometerAlt,
@@ -9,7 +8,6 @@ import Login from '$/AuthPage/components/user-auth/Login';
 import ResetPassword from '$/AuthPage/components/user-auth/ResetPassword';
 import NewPassword from '$/AuthPage/components/user-auth/NewPassword';
 import HomePage from '$/HomePage/HomePage';
-import expensesIncomePage from '$/expensesIncomePage/expensesIncomePage';
 import CategoriesPage from '$/categoriesPage/CategoriesPage';
 import CreateCategoriesPage from '$/createCategories/CreateCategoriesPage';
 
