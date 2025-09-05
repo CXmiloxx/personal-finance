b21205d1f8ae800ce9a1821150bfe52b6cc40554
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-09-05 06:31:27 -0500

    ♻️ refactor(TransactionCard): reorganiza la presentación de la descripción y categoría
    - Se invierte el orden de visualización entre la descripción de la transacción y el nombre de la categoría.

--- src/pages/transactionsPage/components/TransactionCard/TransactionCard.jsx
+++ src/pages/transactionsPage/components/TransactionCard/TransactionCard.jsx
@@ -20,9 +20,9 @@ export default function TransactionCard({ transaction, onEdit, onDelete, viewMod
   const formatFrequency = (frequency) => {
     switch (frequency) {
       case 'deal':
-        
+
         break;
-    
+
       default:
         break;
     }
@@ -40,11 +40,11 @@ export default function TransactionCard({ transaction, onEdit, onDelete, viewMod
             )}
           </div>
           <div>
-            <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-              {transaction.description || 'Sin descripción'}
-            </h3>
-            <p className="text-sm text-gray-500 dark:text-gray-400">
+            <h3 className="text-sm text-gray-500 dark:text-gray-400">
               {transaction.Category?.name || 'Sin categoría'}
+            </h3>
+            <p className="text-lg font-semibold text-gray-800 dark:text-white">
+              {transaction.description || 'Sin descripción'}
             </p>
           </div>
         </div>
